c0396a9f0e6a36c8607dfa3f692d4ddc90be8572
Preslav977 <p.cvetanow@gmail.com>
2024-12-30 09:17:01 +0200

    Added the dependecies in user.test.js file with passport, and session in order to create tests for the user

--- tests/user.test.js
+++ tests/user.test.js
@@ -2,6 +2,128 @@ require("dotenv").config();
 
 const { PrismaClient } = require("@prisma/client/extension");
 
+const request = require("supertest");
+
+const express = require("express");
+
+const LocalStrategy = require("passport-local").Strategy;
+
+const passport = require("passport");
+
+const session = require("express-session");
+
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
+
+const bcrypt = require("bcryptjs");
+
+const userRouter = require("./routes/userRouter");
+
+const app = express();
+
+app.use(
+  session({
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+    secret: process.env.sessionSecret,
+    resave: false,
+    saveUninitialized: false,
+    store: new PrismaSessionStore(new PrismaClient(), {
+      checkPeriod: 2 * 60 * 1000,
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
+);
+
+app.use(passport.session());
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await prisma.user.findFirst({
+        where: {
+          username: username,
+        },
+      });
+
+      // console.log(user);
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      const match = await bcrypt.compare(password, user.password);
+      // console.log(match, password, user.password);
+      if (!match) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    // console.log(user);
+
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+app.post(
+  "/users/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/",
+  })
+);
+
+app.get(
+  "/users/logout",
+  verifyToken,
+  asyncHandler(async (req, res, next) => {
+    req.logout((err) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/user/login");
+    });
+
+    await prisma.user.update({
+      where: {
+        id: req.authData.id,
+      },
+      data: {
+        online_presence: "OFFLINE",
+      },
+    });
+  })
+);
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/users", userRouter);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Express app - listening on port ${PORT}!`));
+
 const databaseUrl =
   process.env.NODE_ENV === "test"
     ? process.env.TEST_DATABASE_URL
@@ -14,3 +136,5 @@ const prisma = new PrismaClient({
     },
   },
 });
+
+test("users should able to sign up", async () => {});
